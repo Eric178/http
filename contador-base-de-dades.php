8f3d9c7104ea093277d05e6e846f4967dd1214fa
Eric178 <ericflorido@gmail.com>
2025-01-09 16:11:09 +0100

    Update contador-base-de-dades.php

--- contador-base-de-dades.php
+++ contador-base-de-dades.php
@@ -1,3 +1,4 @@
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -23,3 +24,5 @@ $row = mysqli_fetch_array($resultat);
 echo $row[0];
 
 $conn->close();
+?>
+
