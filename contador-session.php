8ab86cafacada35b518001da128b54cebb3e70d7
Eric178 <ericflorido@gmail.com>
2025-01-09 16:41:54 +0100

    Update contador-session.php

--- contador-session.php
+++ contador-session.php
@@ -11,20 +11,3 @@ $_SESSION['num'] = ++$num;
 echo $num;
 ?>
 
-contador-fitxer.php
-<?php
-$contador = 0;
-
-if ($fd = fopen('counter.txt', 'r')) {
-    $contador = fgets($fd);
-    fclose($fd);
-}
-
-$fd2 = fopen('counter.txt', 'w');
-$contador = $contador + 1;
-fwrite($fd2, $contador);
-fclose($fd2);
-
-echo $contador;
-?>
-
